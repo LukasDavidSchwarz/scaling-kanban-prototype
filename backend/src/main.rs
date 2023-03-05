491c794b3a2188bf117da13bed87ff5a44897481
Lukas Schwarz <58810022+LukasDavidSchwarz@users.noreply.github.com>
2023-03-05 22:42:04 +0100

    Add hello world axum endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,22 @@
+use axum::http::StatusCode;
+use axum::{extract::State, routing::get, Json, Router, ServiceExt};
 use mongodb::{options::ClientOptions, Client};
 use std::env;
+use std::net::SocketAddr;
+use std::sync::Arc;
 use std::time::Duration;
 
 pub struct AppState {
     db: Client,
 }
 
+// TODO: use structopt for environment variable parsing
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    println!("Starting server...");
 
     let db_uri =
         env::var("DB_CONNECTION_URI").expect("Environment variable ´DB_CONNECTION_URI´ not set!");
-
     let mut client_options = ClientOptions::parse(db_uri)
         .await
         .expect("Failed to parse environment variable ´DB_CONNECTION_URI´");
@@ -22,9 +26,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Database names:");
     for db_name in db.list_database_names(None, None).await? {
-        println!("{}", db_name);
+        println!(" - {}", db_name);
     }
 
-    println!("Backend finished executing.");
+    let shared_state = Arc::new(AppState { db });
+    let app = Router::new()
+        .route("/", get(index_handler))
+        .with_state(shared_state);
+
+    let port = env::var("BACKEND_PORT")
+        .expect("Environment variable `BACKEND_PORT` not set!")
+        .parse()
+        .expect("Failed to parse `BACKEND_PORT`!");
+
+    // TODO: parse ip address from environment
+    let address = SocketAddr::from(([0, 0, 0, 0], port));
+    println!("Listening on http://{}...", address);
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await?;
+
+    println!("Server stopped.");
     Ok(())
+}
+
+async fn index_handler() -> &'static str {
+    "Hello, World!"
 }
