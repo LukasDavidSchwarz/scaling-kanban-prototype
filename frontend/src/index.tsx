4078df6ae70bd5b08077da84d7dc9382a748c437
Lukas Schwarz <58810022+LukasDavidSchwarz@users.noreply.github.com>
2023-03-27 23:45:39 +0200

    Remove react strict mode to fix drag and drop

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,8 +6,7 @@ const rootElement = document.getElementById('root') as HTMLElement;
 if (!rootElement) throw new Error('Root container missing in index.html');
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+// TODO: Use strict mode once react-beautiful-dnd supports it for react18
+//  (see https://github.com/atlassian/react-beautiful-dnd/issues/2407)
+//  or it is replaced by another drag and drop solution
+root.render(<App />);
