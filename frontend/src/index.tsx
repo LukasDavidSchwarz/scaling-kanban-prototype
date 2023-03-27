e0913a1306a46cb85e2e9cb637b0be835a4b5648
Lukas Schwarz <58810022+LukasDavidSchwarz@users.noreply.github.com>
2023-03-27 23:16:44 +0200

    Update index.tsx to React18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root') as HTMLElement;
+if (!rootElement) throw new Error('Root container missing in index.html');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
