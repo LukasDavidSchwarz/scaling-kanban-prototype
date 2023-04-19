c3bb0bb65832e9d1357f9a8ae173ce18951def40
Lukas Schwarz <58810022+LukasDavidSchwarz@users.noreply.github.com>
2023-04-19 19:55:17 +0200

    Fix React key attribute of BoardList

--- frontend/src/BoardList.tsx
+++ frontend/src/BoardList.tsx
@@ -37,12 +37,8 @@ export default function BoardList() {
                 <h2 className="pt-5 pb-3 boards-headline text-center">Boards:</h2>
                 <div className="row row-cols-1 row-cols-md-3">
                     {state.boards.map((board) => (
-                        <div className="col mb-4">
-                            <Link
-                                key={board.id}
-                                className="card board-link"
-                                to={`/boards/${board.id}`}
-                            >
+                        <div key={board.id} className="col mb-4">
+                            <Link className="card board-link" to={`/boards/${board.id}`}>
                                 <div className="mb-3 card-body">{board.name}</div>
                             </Link>
                         </div>
